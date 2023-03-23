69fa64a68dcab48e95482be9ec937535cf5f79d1
pratiksha <pratikshashelke72717@gmail.com>
2023-03-23 09:30:59 +0530

    Holiday File

--- LeaveManagement/Holidays.sql
+++ LeaveManagement/Holidays.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[Holidays]
 (
-	[day] INT NOT NULL PRIMARY KEY, 
+	[Holiday_id] INT NOT NULL PRIMARY KEY, 
+    [day] INT NOT NULL, 
     [wday] INT NOT NULL, 
     [month] INT NOT NULL, 
     [year] INT NOT NULL, 
-    [reason] VARBINARY(MAX) NULL
+    [reason] VARCHAR(MAX) NOT NULL, 
+    [State_id] INT NOT NULL, 
+    [Country_id] INT NOT NULL
 )
